2c237d05323ca68e62efd55cd02b4621d8fe1176
kiranmicrois <34733172+kiranmicrois@users.noreply.github.com>
2021-03-28 19:17:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,106 @@
+'use strict';
 
-let history = document.querySelector(".history");
-let result = document.querySelector(".result");
+const btnNum = document.querySelectorAll('.btn_num');
+const btnCalc = document.querySelectorAll('.btn_calc');
+const curValue = document.querySelector('.current_value');
+const history = document.querySelector('.history_value');
+const btnClearAll = document.querySelector('.btn_clear');
+const btnClear = document.querySelector('.btn_del');
+const btnEvaluate = document.querySelector('.btn_evaluate');
 
-function num(x){
-    
-    history.innerHTML += x;
-     
+let resultDisplayed = false;
+let lastValue = function () {
+  return history.innerHTML.slice(-1);
+};
 
-}   
+//numbers
+btnNum.forEach(num => {
+  num.addEventListener('click', e => {
+    //default
+    if (resultDisplayed == false) {
+      curValue.textContent =
+        curValue.textContent == 0 ? ' ' : curValue.textContent;
 
-function decimal(z){
-
-    history.innerHTML += z ;
-    if(history.innerHTML == z){
-        z = ""
+      curValue.textContent += num.textContent;
+      new Intl.NumberFormat('en-IN').format(curValue.textContent);
+      lastValue();
     }
+    //prssing operator will continue with current calculation
+    else if (
+      (resultDisplayed == true && lastValue() === '*') ||
+      lastValue() === '+' ||
+      lastValue() === '-' ||
+      lastValue() === '/'
+    ) {
+      curValue.textContent = '';
 
-}
-   
-function operator(y){
-
-    history.innerHTML += y;
-    
-    if (result.innerHTML > 0) {
-        history.innerHTML = result.innerHTML;
-        history.innerHTML += y;
-        result.innerHTML= "";
+      curValue.textContent += num.textContent;
+      resultDisplayed = false;
     }
-}
+    //pressing number star a new calculation
+    else {
+      resultDisplayed = false;
+      history.textContent = ' ';
+      curValue.textContent = 0;
+      curValue.textContent =
+        curValue.textContent == 0 ? ' ' : curValue.textContent;
 
-function remove(){
-    
-    history.innerHTML = "";
-    result.innerHTML = "";
-    
-    
-    
-}
-
-function backSpace(){
-    
-    history.innerHTML = history.innerHTML.substr(0,history.innerHTML.length-1);
-}
-
-function calculate(){
-    var x = history.innerHTML;
-    var x = eval( x );
-    result.innerHTML = formattedNumber(x);
-    function formattedNumber(x) {
-        let n = Number(x);
-        let value = n.toLocaleString("en");
-        return value = x;
-    
-    } 
-
-    if(x>0){
-        history.innerHTML = "";
+      curValue.textContent += num.textContent;
     }
-}
+  });
+});
+//operators
+btnCalc.forEach(operators => {
+  operators.addEventListener('click', e => {
+    let test = history.textContent.slice(0, history.textContent.length - 1);
+    //it avoid adding multiple operator contineously
+    if (history.textContent.includes(operators.textContent)) {
+      history.textContent = !isNaN(lastValue())
+        ? curValue.textContent + operators.textContent
+        : history.textContent;
+      console.log(lastValue(), resultDisplayed, test);
+    }
+    //replace the operator
+    else if (
+      lastValue() === '*' ||
+      lastValue() === '+' ||
+      lastValue() === '-' ||
+      lastValue() === '/'
+    ) {
+      history.textContent = test + operators.textContent;
+    } else {
+      history.textContent = ' ';
+      history.textContent += curValue.textContent + ' ' + operators.textContent;
+      curValue.textContent = ' ';
+    }
+  });
+});
+
+//clears everything
+btnClearAll.addEventListener('click', () => {
+  history.textContent = ' ';
+  curValue.textContent = 0;
+  resultDisplayed = resultDisplayed == false ? true : false;
+});
+
+//clear last value per click in result area
+btnClear.addEventListener('click', () => {
+  let curLength = curValue.textContent.length;
+  console.log(curLength);
+  curValue.textContent = curValue.textContent.slice(0, curLength - 1);
+});
+
+// evaluate
+
+btnEvaluate.addEventListener('click', () => {
+  history.textContent += ' ' + curValue.textContent;
+  curValue.textContent = eval(history.textContent);
+
+  //for decimal point shows max upto 4 decimal points if decimal result occurs
+  curValue.textContent = curValue.textContent.includes('.')
+    ? eval(curValue.textContent).toFixed(4)
+    : eval(curValue.textContent);
+
+  resultDisplayed = true;
+  console.log(lastValue(), resultDisplayed);
+});
